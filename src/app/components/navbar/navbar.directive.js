0845798d2ff20a11092853cddddc004ab50574b7
Kelvin <yifeng.liang@accenture.com>
2016-12-01 12:41:41 +0800

    fast commit

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -11,7 +11,7 @@
       templateUrl: 'app/components/navbar/navbar.html',
       scope: {},
       /** @ngInject */
-      controller: function ($state, $cookies,toastr) {
+      controller: function ($state, $cookies, toastr, f321Api) {
         var vm = this;
         vm.short = [
           {
@@ -24,7 +24,33 @@
           $cookies.remove('autoLogin');
           $state.go('login');
           return toastr.info('你已经安全退出', '操作成功');
-        }
+        };
+        vm.params = {
+          crmContactList: []
+        };
+        vm.addContact = function () {
+          vm.params.crmContactList.push({
+            name: '',
+            sex: '',
+            mobile: '',
+            tel: '',
+            dept: '',
+            email: '',
+            fax: '',
+            isPrimary: false
+          });
+        };
+        vm.remove = function (contact) {
+          vm.params.crmContactList.splice(vm.params.crmContactList.indexOf(contact), 1);
+        };
+        vm.submit = function () {
+          f321Api.customer$save(vm.params).then(function () {
+            toastr.success('添加客户信息成功~', '操作成功');
+            $state.go(-1);
+          }, function (err) {
+            toastr.error(err, '出错');
+          });
+        };
       },
       controllerAs: 'bar',
       bindToController: true
